15b82013b70f66b135c744d2d484d699215de0ed
Eugene Kubesh <e.kubesh@wis.eu.com>
2019-01-03 01:33:39 +0300

    updated apiUrl for prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,6 +2,6 @@ import { Environment } from './model';
 
 
 export const environment: Environment = {
-  apiUrl: 'http://localhost:8000',
+  apiUrl: 'https://office.wis.software',
   production: true,
 };
